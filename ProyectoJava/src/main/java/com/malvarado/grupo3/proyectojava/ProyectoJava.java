241899a2a19376b7a987d4af56a4a6d4c2e457b0
MarcoAlv <marcs2805@gmail.com>
2023-03-30 19:59:30 -0500

    Reglas

--- ProyectoJava/src/main/java/com/malvarado/grupo3/proyectojava/ProyectoJava.java
+++ ProyectoJava/src/main/java/com/malvarado/grupo3/proyectojava/ProyectoJava.java
@@ -12,10 +12,23 @@ package com.malvarado.grupo3.proyectojava;
 public class ProyectoJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This is the first change");
         
-        int x = 10;
+        /**
+         * 
+         * REGLAS AL PROGRAMAR:
+         * 
+         * - Escribir comentarios en español al crear funciones
+         *
+         * - Los nombes de las variables se escriben DeEstaForma;
+         *   con mayúsculas en cada palabra
+         * 
+         * - Comentar solo lo necesario
+         * 
+         * - Avanzar al menos 3 horas semanales
+         * 
+         * si no saben algun tema pueden preguntar :3 happy to help
+         * 
+         */
         
         
     }
